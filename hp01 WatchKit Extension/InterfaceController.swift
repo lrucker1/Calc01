d081681ba9d3d3e4f7b9f58bd445a8fd9cc58176
Lee Ann Rucker <lrucker@macpro.local>
2017-08-08 13:03:36 -0700

    Initial Commit

---
+++ hp01 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  hp01 WatchKit Extension
+//
+//  Created by Lee Ann Rucker on 8/8/17.
+//  Copyright © 2017 Lee Ann Rucker. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ hp01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hp01
+//
+//  Created by Lee Ann Rucker on 8/8/17.
+//  Copyright © 2017 Lee Ann Rucker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
